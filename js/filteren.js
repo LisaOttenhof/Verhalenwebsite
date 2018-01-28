e7f72368af4404be71b4fe015f2b4035fc0822ec
Lisa Ottenhof <lisaottenhof@Lisas-MacBook-Pro.local>
2018-01-28 21:36:29 +0100

    28 jan 21:36

--- js/filteren.js
+++ js/filteren.js
@@ -1,7 +1,7 @@
 
 //---------- filter leesduur ----------
-var leesduur_button = document.querySelector('section>form>fieldset>div:nth-of-type(1)>button');
-var leesduur_show = document.querySelector('section>form>fieldset>div:nth-of-type(1)>div');
+var leesduur_button = document.querySelector('section>form>div:nth-of-type(1)>fieldset>div:nth-of-type(1)>button');
+var leesduur_show = document.querySelector('section>form>div:nth-of-type(1)>fieldset>div:nth-of-type(1)>div');
 
 function leesduur_functie() {
   leesduur_show.classList.toggle('leesduur_dropdown');
@@ -13,8 +13,8 @@ leesduur_button.addEventListener('click', leesduur_functie);
 
 
 //---------- filter emotie ----------
-var emotie_button = document.querySelector('section>form>fieldset>div:nth-of-type(2)>button');
-var emotie_show = document.querySelector('section>form>fieldset>div:nth-of-type(2)>div');
+var emotie_button = document.querySelector('section>form>div>fieldset>div:nth-of-type(2)>button');
+var emotie_show = document.querySelector('section>form>div>fieldset>div:nth-of-type(2)>div');
 
 function emotie_functie() {
   leesduur_show.classList.remove('leesduur_dropdown');
--- js/sorteren.js
+++ js/sorteren.js
@@ -1,7 +1,7 @@
 
 //---------- sorteren ----------
-var sorteren_button = document.querySelector('section>form>fieldset:nth-of-type(3)>div:nth-of-type(1)>button');
-var sorteren_show = document.querySelector('section>form>fieldset:nth-of-type(3)>div:nth-of-type(1)>div');
+var sorteren_button = document.querySelector('section>form>div:nth-of-type(1)>fieldset:nth-of-type(2)>div:nth-of-type(1)>button');
+var sorteren_show = document.querySelector('section>form>div:nth-of-type(1)>fieldset:nth-of-type(2)>div:nth-of-type(1)>div');
 
 function sorteren_functie() {
   leesduur_show.classList.remove('leesduur_dropdown');
